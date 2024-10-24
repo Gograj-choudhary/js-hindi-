465e2b821a490aee4f3e0225b61136aa64209e5a
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2024-10-24 14:38:37 +0000

    commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,23 @@
+const employId = 2323
+let employEmail = "Gograj@gmail.com"
+var employPassword = 34323
+employCity = "jaipur"
+
+
+// employId = 3232 
+/* we know that employId already stored in const variable so it can not be further changed like this,But other variable can changed */
+
+console.log(employId); // this a way to print of any variable in js
+// now changing vales of varibles 
+
+employEmail = "Ram@1234"
+employPassword = 1234
+employCity = "dheli"
+let employState; // if we declare a variable but not give any value then it show undifined in printing
+
+// avoid to use of var variable 
+
+
+console.table([employId, employEmail, employPassword, employCity, employState] ); // this also a good way to print varibles . it is also a easy and fast way
+
+
