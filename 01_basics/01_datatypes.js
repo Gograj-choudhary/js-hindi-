70b24908cba388f9609e244bb56f75f3db1a5442
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2024-10-24 14:32:26 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+ let isloggedin = false
+ let state;
+
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean => true/false
+ // null => standelone value(norlmaly kgali hoti hai)
+ // undefined => when you created a verible but not give any value
+ // symbol = unique 
+
+ // object
+ console.log(typeof "Gograj") // it gives string 
+ console.log(typeof "undefiend") // it gives undefiend 
+ console.log(typeof "null") // it gives object 
