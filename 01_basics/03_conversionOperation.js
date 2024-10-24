2a9e0d58e07bae479e1a376ec485ed43409f6892
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2024-10-24 18:41:04 +0000

    operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = "11" // it will gives a string type data
+
+// console.log(typeof score);
+
+// if score = " 112abc"  then it will gives NaA(not a number)
+
+
+let isLoggedIn = "Gograj"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ;  0 => false
+// "" = false
+// "Gograj" = true
+
+let somenumber = 12
+
+let stringNumber = string(somenumber)
+ // console.log(typeof stringNumber);
+
+ // *********************** Operations ***********//
+
+let val = 3
+let negval = -val
+//console.log(negval) it will give -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); power 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Gograj"
+let str2 = "choudhary"
+//let str = str1 + str2; it will gives Gograjchoudhary
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); ==> 122 
+// console.log(1+2 +"2"); ==> 32
+ 
