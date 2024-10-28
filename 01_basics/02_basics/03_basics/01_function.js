7f9b8d69097a217bcdc45d7f03ab4eeec422bc8e
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2024-10-28 18:32:03 +0000

    update

--- 01_basics/02_basics/03_basics/01_function.js
+++ 01_basics/02_basics/03_basics/01_function.js
@@ -30,5 +30,55 @@ addTwoNumber(3,null); //==> it will gives 3
 function addTwoNumber(num1 , num2){
     let result = num1 + num2
     return result 
-    console.log("Gograj") // ==> it will not be print because it exucatio
+    // console.log("Gograj") // ==> it will not be print because it exucation after return
+
 }
+const result = addTwoNumber(2,4)
+console.log("result:", result);
+
+function loginUserMessage(username){
+    if(username === undefined){ // also use like if(!username)
+        console.log("pleas eenter a username");
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage())
+
+
+// *******  functions *********//
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000)) // it willl give a array of num1s
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000)) // it will gives array of num1 but only 500 and 2000
+
+const user = {
+    username: "hitesh",
+    price: 199
+
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
